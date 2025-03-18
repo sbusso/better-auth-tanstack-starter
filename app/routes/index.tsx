e87d811b91e0a7a9749db986fefb1770cddb8592
daveycodez <david@swiftapk.com>
2025-03-19 00:59:20 +0100

    shadcn

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,15 +8,17 @@ export const Route = createFileRoute("/")({
 function App() {
     return (
         <div className="text-center">
-            <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-[calc(10px+2vmin)] text-white">
+            <header className="flex min-h-svh flex-col items-center justify-center bg-[#282c34] text-[calc(10px+2vmin)] text-white">
                 <img
                     src={logo}
                     className="pointer-events-none h-[40vmin] animate-[spin_20s_linear_infinite]"
                     alt="logo"
                 />
+
                 <p>
                     Edit <code>src/routes/index.tsx</code> and save to reload.
                 </p>
+
                 <a
                     className="text-[#61dafb] hover:underline"
                     href="https://reactjs.org"
@@ -25,6 +27,7 @@ function App() {
                 >
                     Learn React
                 </a>
+
                 <a
                     className="text-[#61dafb] hover:underline"
                     href="https://tanstack.com"
