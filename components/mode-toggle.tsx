c632b4134348c88e1777ce45a822c9e56df1a993
daveycodez <david@swiftapk.com>
2025-03-19 01:56:40 +0100

    Some cleanup

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -23,15 +23,18 @@ export function ModeToggle() {
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
+
             <DropdownMenuContent align="end" onCloseAutoFocus={(e) => e.preventDefault()}>
                 <DropdownMenuItem onClick={() => setTheme("light")}>
                     <SunIcon />
                     Light
                 </DropdownMenuItem>
+
                 <DropdownMenuItem onClick={() => setTheme("dark")}>
                     <MoonIcon />
                     Dark
                 </DropdownMenuItem>
+
                 <DropdownMenuItem onClick={() => setTheme("system")}>
                     <MonitorIcon />
                     System
